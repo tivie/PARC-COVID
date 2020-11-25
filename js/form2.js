501ff0e6f35d4d6684b757c8f890c575e23a3ea5
Estevão Soares dos Santos <estevao.santos@gmail.com>
2020-11-25 18:13:58 +0000

    retirar debug

--- js/form2.js
+++ js/form2.js
@@ -168,9 +168,9 @@ function submitForm(ev) {
   }
   
   // para testar, abrir a consola no browser e descomentar as 2 linhas abaixo
-  console.log(payload);
-  console.log(JSON.stringify(payload));
-  return true;
+  //console.log(payload);
+  //console.log(JSON.stringify(payload));
+  //return true;
 
   var url = config.form2.url;
 
